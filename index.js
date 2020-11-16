0743bb2519c0c46aaba3e789e3cf53d00ab43afd
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-16 16:15:12 -0500

    Sum Array of Numbers

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// FUNCTION IMPLEMENTATION
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let n = 0;
+  array.forEach((element) => {
+    if (Array.isArray(element)) {
+      n += sumItems(element);
+    } else {
+      n += element;
+    }
+  });
+  return n;
 }
 
+// EXPORT FILE
 module.exports = sumItems;
